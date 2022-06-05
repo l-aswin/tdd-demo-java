83cfec4d0f5aee089abf8445529e2df93d2af9ba
aswin <l.aswin4@gmail.com>
2022-06-05 07:01:44 +0530

    modified add method to allow multiple delimiters given in add method argument

--- src/com/aswin/StringCalculator.java
+++ src/com/aswin/StringCalculator.java
@@ -2,26 +2,63 @@ package com.aswin;
 
 import com.aswin.assertions.NegativeInputException;
 
+import java.util.regex.Pattern;
+
 public class StringCalculator {
-    private static int countAddMethodCall=0;
+    private static int countAddMethodCall = 0;
+
     public int add(String numbers) throws NegativeInputException {
         countAddMethodCall++;
-        if (numbers.matches("//(.*)\n(.*)")) {
+        String specialCharacters = "\\.[]{}()<>*+-=!?^$|";
+
+        if (numbers.matches("//\\[(.*)](.*)\n(.*)")) {
+            //split user input into lines
+            String[] lines = numbers.split("\n");
+            //extract array of delimiters from first line
+            String[] delimiterArray = Pattern.compile("\\[(.*?)]")
+                    .matcher(lines[0])
+                    .results()
+                    .map(mr -> mr.group(1))
+                    .toArray(String[]::new);
+            //loop through array of delimiters
+            StringBuilder delimiterExpression= new StringBuilder();
+            for(String item:delimiterArray){
+                //add escape sequence to delimiter,if it has reserved symbols
+                StringBuilder temp = new StringBuilder();
+                char[] array = item.toCharArray();
+                for (char c : array) {
+                    if (specialCharacters.contains(String.valueOf(c))) {
+                        temp.append("\\").append(c);
+                    } else {
+                        temp.append(c);
+                    }
+                }
+                //add this to delimiterExpr
+                if(delimiterExpression.toString().equals(""))
+                    delimiterExpression = new StringBuilder(temp.toString());
+                else
+                    delimiterExpression.append("|").append(temp);
+            }
+            //extract numbers from line 2
+            String[] numberArray = lines[1].split(delimiterExpression.toString());
+            //iterate through array and calculate sum
+            return sumNumberArray(numberArray);
+        }
+        else if (numbers.matches("//(.*)\n(.*)")) {
             //split user input into lines
             String[] lines = numbers.split("\n");
 
             //extract delimiter from first line
-            String delimiterUserInput = lines[0].replaceFirst("//", "").replace("[","").replace("]","");
+            String delimiterUserInput = lines[0].replaceFirst("//", "");
 
             //add escape sequence to delimiter,if it has reserved symbols
-            String specialCharacters = "\\.[]{}()<>*+-=!?^$|";
-            StringBuilder delimiter= new StringBuilder();
+            StringBuilder delimiter = new StringBuilder();
             char[] delimiterArray = delimiterUserInput.toCharArray();
-            for(int i=0;i<delimiterArray.length;i++){
-                if( specialCharacters.contains(String.valueOf(delimiterArray[i])) ){
-                    delimiter.append("\\").append(delimiterArray[i]);
-                }else {
-                    delimiter.append(delimiterArray[i]);
+            for (char c : delimiterArray) {
+                if (specialCharacters.contains(String.valueOf(c))) {
+                    delimiter.append("\\").append(c);
+                } else {
+                    delimiter.append(c);
                 }
             }
 
@@ -29,49 +66,39 @@ public class StringCalculator {
             String[] numberArray = lines[1].split(delimiter.toString());
 
             //iterate through array and calculate sum
-            int sum = 0;
-            StringBuilder negativesInInput= new StringBuilder();
-            for (String number : numberArray) {
-                if(Integer.parseInt(number)<0)
-                    negativesInInput.append(number).append(",");
-                else {
-                    //ignore number if it is greater than 1000
-                    if(Integer.parseInt(number)<=1000)
-                        sum += Integer.parseInt(number);
-                }
-            }
+            return sumNumberArray(numberArray);
 
-            //if input has negative number, throw exception
-            if(!negativesInInput.toString().equals(""))
-                throw new NegativeInputException("negatives not allowed " + negativesInInput);
-            else
-                return sum;
         } else if (numbers.equals(""))
             return 0;
         else {
             //extract numbers from string using delimiters , or \n
             String[] numberArray = numbers.split("[,\n]");
             //iterate through array and calculate sum
-            int sum = 0;
-            StringBuilder negativesInInput= new StringBuilder();
-            for (String number : numberArray) {
-                if(Integer.parseInt(number)<0)
-                    negativesInInput.append(number).append(",");
-                else {
-                    //ignore number if it is greater than 1000
-                    if(Integer.parseInt(number)<=1000)
-                        sum += Integer.parseInt(number);
-                }
-            }
-            //if input has negative number, throw exception
-            if(!negativesInInput.toString().equals(""))
-                throw new NegativeInputException("negatives not allowed " + negativesInInput);
-            else
-                return sum;
+            return sumNumberArray(numberArray);
         }
     }
 
     public int getCalledCount() {
         return countAddMethodCall;
+    }
+
+    private int sumNumberArray(String[] numberArray) throws NegativeInputException {
+        //iterate through array and calculate sum
+        int sum = 0;
+        StringBuilder negativesInInput = new StringBuilder();
+        for (String number : numberArray) {
+            if (Integer.parseInt(number) < 0)
+                negativesInInput.append(number).append(",");
+            else {
+                //ignore number if it is greater than 1000
+                if (Integer.parseInt(number) <= 1000)
+                    sum += Integer.parseInt(number);
+            }
+        }
+        //if input has negative number, throw exception
+        if (!negativesInInput.toString().equals(""))
+            throw new NegativeInputException("negatives not allowed " + negativesInInput);
+        else
+            return sum;
     }
 }
--- src/com/aswin/StringCalculatorTest.java
+++ src/com/aswin/StringCalculatorTest.java
@@ -7,41 +7,42 @@ import static org.junit.jupiter.api.Assertions.*;
 
 @TestMethodOrder(MethodOrderer.MethodName.class)
 public class StringCalculatorTest {
-    String msgFailed=" test case failed";
+    String msgFailed = " test case failed";
+
     @Test
     public void addInputEmptyString() throws NegativeInputException {
         StringCalculator stringCalculator = new StringCalculator();
-        assertEquals(0,stringCalculator.add(""),"addInputEmptyString"+msgFailed);
+        assertEquals(0, stringCalculator.add(""), "addInputEmptyString" + msgFailed);
     }
 
     @Test
     public void addInputLengthOne() throws NegativeInputException {
         StringCalculator stringCalculator = new StringCalculator();
-        assertEquals(1,stringCalculator.add("1"),"addInputLengthOne"+msgFailed);
+        assertEquals(1, stringCalculator.add("1"), "addInputLengthOne" + msgFailed);
     }
 
     @Test
     public void addInputLengthTwo() throws NegativeInputException {
         StringCalculator stringCalculator = new StringCalculator();
-        assertEquals(3,stringCalculator.add("1,2"),"addInputLengthTwo"+msgFailed);
+        assertEquals(3, stringCalculator.add("1,2"), "addInputLengthTwo" + msgFailed);
     }
 
     @Test
     public void addInputLengthUnknown() throws NegativeInputException {
         StringCalculator stringCalculator = new StringCalculator();
-        assertEquals(120,stringCalculator.add("1,2,8,9,67,33"),"addInputLengthUnknown"+msgFailed);
+        assertEquals(120, stringCalculator.add("1,2,8,9,67,33"), "addInputLengthUnknown" + msgFailed);
     }
 
     @Test
     public void addInputWithNewline() throws NegativeInputException {
         StringCalculator stringCalculator = new StringCalculator();
-        assertEquals(6,stringCalculator.add("1\n2,3"),"addInputWithNewline"+msgFailed);
+        assertEquals(6, stringCalculator.add("1\n2,3"), "addInputWithNewline" + msgFailed);
     }
 
     @Test
     public void addSupportDifferentDelimiters() throws NegativeInputException {
         StringCalculator stringCalculator = new StringCalculator();
-        assertEquals(3,stringCalculator.add("//;\n1;2"),"addSupportDifferentDelimiters"+msgFailed);
+        assertEquals(3, stringCalculator.add("//;\n1;2"), "addSupportDifferentDelimiters" + msgFailed);
     }
 
     @Test
@@ -53,26 +54,31 @@ public class StringCalculatorTest {
     @Test
     public void addDisallowNegative() throws NegativeInputException {
         StringCalculator stringCalculator = new StringCalculator();
-        stringCalculator.add("-1\n-2,4,5,-6,7");
+        stringCalculator.add("1\n-2,4,5,-6,7");
     }
 
     @Test
-    public void addIgnoreHighNumber() throws NegativeInputException{
+    public void addIgnoreHighNumber() throws NegativeInputException {
         StringCalculator stringCalculator = new StringCalculator();
-        assertEquals(2,stringCalculator.add("2,1001"),"Ignore number>1000"+msgFailed);
+        assertEquals(2, stringCalculator.add("2,1001"), "Ignore number>1000" + msgFailed);
     }
 
     @Test
-    public void addDelimiterStringLengthNoLimit() throws NegativeInputException{
+    public void addDelimiterStringLengthNoLimit() throws NegativeInputException {
         StringCalculator stringCalculator = new StringCalculator();
-        assertEquals(10, stringCalculator.add("//[***]\n1***2***3***4"),"addDelimiterStringLengthNoLimit"+msgFailed);
+        assertEquals(9, stringCalculator.add("//[***]\n2***3***4"), "addDelimiterStringLengthNoLimit" + msgFailed);
+    }
 
+    @Test
+    public void addUserInputMultipleDelimiter() throws NegativeInputException {
+        StringCalculator stringCalculator = new StringCalculator();
+        assertEquals(13, stringCalculator.add("//[*][%]\n8*2%3"), "addUserInputMultipleDelimiter" + msgFailed);
     }
 
     @Test
     public void testGetCalledCount() {
         StringCalculator stringCalculator = new StringCalculator();
-        assertEquals(10,stringCalculator.getCalledCount(),"testGetCalledCount"+msgFailed);
+        assertEquals(11, stringCalculator.getCalledCount(), "testGetCalledCount" + msgFailed);
     }
 
 }
